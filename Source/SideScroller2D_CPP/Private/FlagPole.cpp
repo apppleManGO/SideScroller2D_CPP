a26855fd2501cc85e18599bec713e565bda63367
apppleManGO <120433796+apppleManGO@users.noreply.github.com>
2024-11-22 20:42:52 +0900

    flag remaster

--- Source/SideScroller2D_CPP/Private/FlagPole.cpp
+++ Source/SideScroller2D_CPP/Private/FlagPole.cpp
@@ -7,10 +7,11 @@
 #include "Player_2d.h"
 #include "PaperSpriteComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Components/TimelineComponent.h"
 
 AFlagPole::AFlagPole()
 {
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 	// 충돌 박스 컴포넌트 생성 및 설정
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
@@ -24,86 +25,90 @@ AFlagPole::AFlagPole()
 
 	// 충돌 이벤트 바인딩
 	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AFlagPole::OnOverlapBegin);
+
+	Timeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline"));
 }
 void AFlagPole::BeginPlay()
 {
-	FlagStartLocation=FlagSprite->GetComponentLocation();
+	Super::BeginPlay();
+
+	if (FlagCurve)
+	{
+		// 타임라인 이벤트에 함수 바인딩
+		FOnTimelineFloat ProgressFunction;
+		ProgressFunction.BindUFunction(this, FName("HandleFlagMovement"));
+		Timeline->AddInterpFloat(FlagCurve, ProgressFunction);
+
+		FOnTimelineEvent TimelineFinishedEvent;
+		TimelineFinishedEvent.BindUFunction(this, FName("OnFlagLowered"));
+		Timeline->SetTimelineFinishedFunc(TimelineFinishedEvent);
+
+
+	}
+}
+
+void AFlagPole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    
+	if (Timeline)
+	{
+		Timeline->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, nullptr);
+	}
+}
+
+void AFlagPole::HandleFlagMovement(float Value)
+{
+	if (FlagSprite)
+	{
+		FVector NewLocation = FlagStartLocation;
+		NewLocation.Z -= (Value * 120.0f); // 60.0f 값으로 조정하여 Z축 이동 범위를 크게 설정
+		FlagSprite->SetWorldLocation(NewLocation);
+	}
+	if (APlayer_2d* PlayerCharacter = Cast<APlayer_2d>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)))  // 플레이어 캐릭터 찾기
+	{
+		// 플레이어 이동
+		FVector NewPlayerLocation = PlayerStartLocation;
+		NewPlayerLocation.Z -= (Value * 120.0f); // 플레이어도 같은 비율로 내려가도록 설정
+		PlayerCharacter->SetActorLocation(NewPlayerLocation);
+		// 플레이어가 지정한 위치에 도달했는지 체크
+                if (NewPlayerLocation.Z <= PlayerEndLocation.Z)
+                {
+                    if (!bTimelineFinished)
+                    {
+                        bTimelineFinished = true;
+                        Timeline->Stop(); // 타임라인 종료
+                        OnFlagLowered(); // 상태 업데이트
+                    }
+                }
+	}
+}
+
+void AFlagPole::OnFlagLowered()
+{
+	bFlagLowered = true; // 상태 업데이트
+	// 이후 캐릭터 이동 처리
 }
 void AFlagPole::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor && OtherActor != this)
 	{
 		APlayer_2d* PlayerCharacter = Cast<APlayer_2d>(OtherActor);
-		if (PlayerCharacter)
+		if (PlayerCharacter && !bFlagLowered)  // 깃발이 이미 내려졌는지 체크
 		{
-			// 플레이어 이동 제한
-			PlayerCharacter->GetCharacterMovement()->DisableMovement();
+			// 플레이어의 시작 위치와 목표 위치 설정
+			PlayerStartLocation = PlayerCharacter->GetActorLocation();
+			PlayerEndLocation = FVector(PlayerStartLocation.X, PlayerStartLocation.Y, -50.0f); // Z축을 -50으로 설정
 
-
-			// 깃발을 타고 내려오는 연출 시작
-			FVector StartLocation = PlayerCharacter->GetActorLocation();
-			FVector EndLocationZ = FVector(StartLocation.X, StartLocation.Y, -41.0f); // 내려가는 위치
-			FVector EndFlagLocationZ = FVector(FlagStartLocation.X, FlagStartLocation.Y, -41.0f); // 깃발 내려가는 위치
-
-
-
-			//FVector EndLocationX = FVector(3073.0f, StartLocation.Y, EndLocationZ.Z); // X축으로 이동하는 위치
-			// 
-			// 일정 시간 동안 위치 이동
-			FTimerHandle TimerHandle;
-			GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this, PlayerCharacter, EndLocationZ, EndFlagLocationZ]()
-				{
-					//플레이어 위치 이동
-					FVector CurrentPlayerLocation = PlayerCharacter->GetActorLocation();
-					FVector NewPlayerLocation = FMath::VInterpTo(CurrentPlayerLocation, EndLocationZ, GetWorld()->GetDeltaSeconds(), 1.0f);
-					PlayerCharacter->SetActorLocation(NewPlayerLocation);
-
-					// 깃발 위치 이동
-					FVector CurrentFlagLocation = FlagSprite->GetComponentLocation();
-					FVector NewFlagLocation = FMath::VInterpTo(CurrentFlagLocation, EndFlagLocationZ, GetWorld()->GetDeltaSeconds(), 1.0f);
-					FlagSprite->SetWorldLocation(NewFlagLocation);
-
-
-					if (FMath::Abs(NewPlayerLocation.Z - EndLocationZ.Z) < 1.0f)
-					{
-						PlayerCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
-						PlayerCharacter->SetActorLocation(FVector(2978,100,-63));
-						//FVector EndLocationX = FVector(3073.0f, 100, -71.0); // Z축 유지, X축으로 이동
-
-						//// X축 이동을 위한 타이머 설정
-						//FTimerHandle TimerHandleX;
-						//GetWorld()->GetTimerManager().SetTimer(TimerHandleX, [this, PlayerCharacter, NewPlayerLocation, EndLocationX]()
-						//	{
-						//		FVector CurrentLocation = PlayerCharacter->GetActorLocation();
-
-						//		// X축으로 자연스럽게 이동
-						//		FVector NewLocationX = FMath::VInterpTo(CurrentLocation, EndLocationX, GetWorld()->GetDeltaSeconds(), 2.0f);
-						//		PlayerCharacter->SetActorLocation(NewLocationX);
-
-						//		// X축 이동이 끝났다면 타이머 종료
-						//		if (FMath::Abs(NewLocationX.X - EndLocationX.X) < 5.0f)
-						//		{
-						//			UKismetSystemLibrary::QuitGame(this, 0, EQuitPreference::Quit, false);
-
-						//		}
-						//	}, 0.03f, true, 0.0f);
-						// Z축 위치 이동 타이머 종료
-						//GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
-					}
-
-				}, 0.01f, true, 0.0f);
-
-			
-			// 특정 방향으로의 이동 벡터
-			FVector Direction = FVector(1.0f, 0.0f, 0.0f); // x축으로 이동
-			PlayerCharacter->AddMovementInput(Direction, 1.0f); // 이동
-
-			// 연출 후 게임 클리어
-			//FTimerHandle ClearTimer;
-			//GetWorld()->GetTimerManager().SetTimer(ClearTimer, [=]()
-			//{
-			//	UGameplayStatics::OpenLevel(this, FName("NextLevel"));
-			//}, 3.0f, false);
+			// 타임라인 재생 시작
+			if (FlagCurve && Timeline)
+			{
+				FlagStartLocation = FlagSprite->GetComponentLocation(); // 시작 위치 저장
+				PlayerStartLocation = PlayerCharacter->GetActorLocation(); // 플레이어의 시작 위치 저장
+				Timeline->Play();
+				
+			}
+			bFlagLowered = true;  // 깃발이 내려졌다고 표시
 		}
 	}
 }
--- Source/SideScroller2D_CPP/Public/FlagPole.h
+++ Source/SideScroller2D_CPP/Public/FlagPole.h
@@ -16,6 +16,7 @@ class SIDESCROLLER2D_CPP_API AFlagPole : public APaperSpriteActor
 public:
 	AFlagPole();
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UBoxComponent* CollisionBox;
 
@@ -25,6 +26,28 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	// **타임라인 관련 변수**
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UTimelineComponent* Timeline; // 타임라인 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UCurveFloat* FlagCurve; // 깃발 이동을 위한 곡선
 
-	FVector FlagStartLocation;
+	// **상태 관리**
+	FVector FlagStartLocation; // 깃발의 초기 위치
+	bool bFlagLowered = false; // 깃발이 내려갔는지 여부
+
+	// 타임라인이 끝났는지 확인하는 변수
+	bool bTimelineFinished = false;
+	FVector PlayerEndLocation;
+	FVector PlayerStartLocation;
+	// **함수**
+	UFUNCTION()
+	void HandleFlagMovement(float Value); // 타임라인 콜백 함수
+
+	UFUNCTION()
+	void OnFlagLowered(); // 타임라인 완료 시 호출 함수
+
+
+	
 };
